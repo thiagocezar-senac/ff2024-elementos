e58c359ef874ae869d3dd96c15870fe13289c8e6
thiagocezar-senac <thiago.tcezar@senacsp.edu.br>
2024-10-03 16:43:25 -0300

    feat: Ex 11 - Pergunte o turno que o usuário estuda.

--- exercicios/ex11/js/script.js
+++ exercicios/ex11/js/script.js
@@ -1,26 +1,20 @@
 function aoClicar() {
-    let produto1 = document.querySelector('#produto1').value;
-    let produto2 = document.querySelector('#produto2').value;
-    let produto3 = document.querySelector('#produto3').value;
+    let turno = document.querySelector('#turnoId').value;
+    let turnoNome = '';
 
-    let valor1 = parseFloat(document.querySelector('#valor1').value);
-    let valor2 = parseFloat(document.querySelector('#valor2').value);
-    let valor3 = parseFloat(document.querySelector('#valor3').value);
-
-    let min = Math.min(valor1, valor2, valor3);
-
-    if (min == valor1) {
-        alert('O Produto mais barato é: ' + produto1 + ' ' + valor1 + '.');
+    switch (turno) {
+        case 'm':
+            turnoNome = "Bom dia!";
+            break;
+        case 'v':
+            turnoNome = "Boa tarde!";
+            break;
+        case 'n':
+            turnoNome = "Boa noite!";
+            break;
     }
 
-    if (min == valor2) {
-        alert('O Produto mais barato é: ' + produto2 + ' ' + valor2 + '.');
-    }
-
-    if (min == valor3) {
-        alert('O Produto mais barato é: ' + produto3 + ' ' + valor3 + '.');
-    }
-
+    alert(`${turnoNome}`);
 }
 
 let botao = document.querySelector('.btnExibaValor');
